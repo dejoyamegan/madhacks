5186bd5091c2e340091f6285b11a0f9e03958106
adrii4 <adri.regalado@live.com>
2019-10-19 18:21:07 -0500

    Started storing elements into array

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,29 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+
+function getResults() {
+	//Returns the category selection
+	var e = document.getElementById("demo-category");
+	//Returns the movie genre that the user selected
+	var selectedGenre = e.options[e.selectedIndex].text;
+
+	//Read local file
+	const fs = require('fs');
+	const papa = require('papaparse');
+	const file = fs.createReadStream('movies.csv');
+	var count = 0; // cache the running count
+	papa.parse(file, {
+		worker: true, // Don't bog down the main thread if its a big file
+		step: function(result) {
+			typeof result
+		},
+		complete: function(results, file) {
+			console.log('parsing complete read', count, 'records.'); 
+		}
+	});
+	var	movie1 = getMovie();
+		$body = $('body');
+
+}
